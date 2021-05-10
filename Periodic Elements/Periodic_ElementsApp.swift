8443423c2fa3046fd2739836f97d4fb00aadf7e6
Zuhair Ali <zuhairali20003@gmail.com>
2021-05-09 23:00:56 -0500

    Initial Commit

---
+++ Periodic Elements/Periodic_ElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Periodic_ElementsApp.swift
+//  Periodic Elements
+//
+//  Created by Student on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Periodic_ElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
